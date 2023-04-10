fdfd244a71bf9760021d2b9273553046fd9c0266
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-04-10 16:02:56 +0100

    mapping with quick

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,11 +5,19 @@ import type { DocumentHead } from '@builder.io/qwik-city';
 
 
 export default component$(() => {
+
+  // use signal is fine for 1 value 
   const name = useSignal('martin')
 
   // if you need to update an object use usestore, because when we update it its not going to be reactive with usesignal
 
   const person = useStore({name: "martin", age: 32})
+
+  const blogs = useStore([
+    {id:1, title:"hi"},
+    {id:2, title:"bye"},
+    {id:3, title:"cya"},
+  ])
   return (
     <>
 
@@ -20,6 +28,10 @@ export default component$(() => {
       <button onClick$={() =>name.value = 'bowser'}>Click me </button>
 
       <button onClick$={() =>person.name = 'lugi'}>Click me again</button>
+
+      {blogs.map(blog => (
+        <div key={blog.id}>{blog.title}</div>
+      ))}
       </div>
     </>
   );
