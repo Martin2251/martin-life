4bb5aefcf9798a478368031bf2aee4a788cef636
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-04-10 21:37:57 +0100

    forms

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -1,6 +1,6 @@
 // every route needs its own folder and file called index.tsx
 
-import { component$, useSignal, useStylesScoped$ } from "@builder.io/qwik";
+import { component$, useSignal, useStore, useStylesScoped$ } from "@builder.io/qwik";
 
 // the dollar sign spilts it on chunks its code spiltting on steroids
 
@@ -11,6 +11,9 @@ export default component$(() => {
 
     const formVisible = useSignal(false)
     // you have to use .value to access it
+
+    // form because  multiple values 
+    const formState = useStore({name: "", message: ""})
    
     return(
         <article>
@@ -19,18 +22,31 @@ export default component$(() => {
            <button onClick$={() => formVisible.value = true}>contact me</button>
            {/* conditional render similar to react */}
            {formVisible.value &&(
-                <form>
+            // prevent default:submit is needed as form submit conventionally
+                <form preventdefault:submit onSubmit$={() => {
+                    console.log(formState.name, formState.message)
+                    formState.name=""
+                    formState.message=""
+                }}>
                 <label>
                     <span>Your Name</span>
                     <input
+                    value={formState.name}
                     type="type"
+                    onInput$={(e) =>formState.name = (e.target as HTMLInputElement).value}
                     />
                 </label>
                 <label>
                     <span> Your Message:</span>
-                    <textarea></textarea>
+                  <textarea
+                    onInput$={(e) =>formState.message = (e.target as HTMLInputElement).value}
+                    value={formState.message}
+                    ></textarea>
                 </label>
                 <button>Send</button>
+
+                <p>{formState.name}</p>
+                <p>{formState.message}</p>
                 </form>)}
         </article>
     )
