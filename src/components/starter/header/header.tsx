18b3cc4c5c13da4255cebee4b7a4e98ea4599b89
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-04-10 14:05:18 +0100

    css sorted

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -1,14 +1,29 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useStyles$ } from '@builder.io/qwik';
 import { Link } from '@builder.io/qwik-city';
-import { QwikLogo } from '../icons/qwik';
-import styles from './header.module.css';
+
+import styles from './header.css?inline';
 
 export default component$(() => {
+  useStyles$(styles)
   return (
     <header>
       <nav>
         <img src="/mario_block.jpeg" alt="logo"  />
         <h1>Martin Life</h1>
+        <ul>
+            <li>
+              <Link href='/'>Home</Link>
+            </li>
+            <li>
+              <Link href='/about'>About</Link>
+            </li>
+            <li>
+              <Link href='/contact'>Contact</Link>
+            </li>
+            <li>
+              <Link href='/'></Link>
+            </li>
+        </ul>
       </nav>
     </header>
   );
