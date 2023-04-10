13d34263882e08f26fc3620576f84defd3a5e611
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-04-10 18:06:53 +0100

    conditional render

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -1,6 +1,6 @@
 // every route needs its own folder and file called index.tsx
 
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import { component$, useSignal, useStylesScoped$ } from "@builder.io/qwik";
 
 // the dollar sign spilts it on chunks its code spiltting on steroids
 
@@ -8,12 +8,30 @@ import ContactStyles from "./contact.css?inline"
 
 export default component$(() => {
     useStylesScoped$(ContactStyles)
+
+    const formVisible = useSignal(false)
+    // you have to use .value to access it
    
     return(
         <article>
             <h2>Contact</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, ducimus!</p>
-           
+           <button onClick$={() => formVisible.value = true}>contact me</button>
+           {/* conditional render similar to react */}
+           {formVisible.value &&(
+                <form>
+                <label>
+                    <span>Your Name</span>
+                    <input
+                    type="type"
+                    />
+                </label>
+                <label>
+                    <span> Your Message:</span>
+                    <textarea></textarea>
+                </label>
+                <button>Send</button>
+                </form>)}
         </article>
     )
 })
